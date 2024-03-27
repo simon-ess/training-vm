d49091849faa4a17a3e9b07009f9510816faaaf9
Ralph Lange <ralph.lange@gmx.de>
2024-03-27 12:36:20 +0100

    Add update.sh; improve bootstrap_redhat.sh

--- bootstrap_redhat.sh
+++ bootstrap_redhat.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # bootstrap.sh
 
-# Simple bootstrap script for Rocky 9
+# Simple bootstrap script for RedHat family systems (developed on Rocky 9)
 # - installs prerequisites: git, ansible
 # - clones training-vm repo into ./bootstrap
 
@@ -30,5 +30,15 @@ if [ ! -e "${BOOTSTRAP_DIR}" ]; then
     echo "Cloning training-vm configuration into ./${BOOTSTRAP_DIR}..."
     git clone ${BOOTSTRAP_REPO} ${BOOTSTRAP_DIR}
 else
-    echo "Using existing training-vm configuration in ./${BOOTSTRAP_DIR}."
+    echo "You can update the existing training-vm configuration using 'git pull'."
 fi
+
+# Point out missing local.yml configuration
+if [ ! -e "${BOOTSTRAP_DIR}/ansible/group_vars/local.yml" ]; then
+    echo "Inside ${BOOTSTRAP_DIR}/ansible/group_vars create a local configuration file local.yml"
+    echo "by making a copy of local.yml.sample and editing to your needs."
+else
+    echo "Verify your existing local configuration in ${BOOTSTRAP_DIR}/ansible/group_vars/local.yml"
+fi
+
+echo "Run ${BOOTSTRAP_DIR}/update.sh to update your system."
---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# update.sh
+
+# Simple caller script for ansible-playbook
+# Improved robustness:
+# - tries the first command line arg, then the script directory, then "." as location
+# - remaining command line args are passed through to ansible-playbook call
+
+bootstrap_dir=${1%/}
+script_dir=$(dirname $0)
+
+if [ -d "${bootstrap_dir}" ]; then
+    shift
+elif [ -d "${script_dir}" ]; then
+    bootstrap_dir=${script_dir%/}
+else
+    bootstrap_dir="."
+fi
+
+if [ ! -d ${bootstrap_dir}/ansible ]; then
+    echo "update.sh [bootstrap_dir] [ansible-playbook args...]"
+    exit 1
+fi
+
+exec ansible-playbook -i ${bootstrap_dir}/ansible/hosts ${bootstrap_dir}/ansible/playbook.yml "$@"
