976e2c9720fead891ad0534e7d3e43d9d1d98dbd
Ralph Lange <ralph.lange@gmx.de>
2025-02-02 16:28:45 +0100

    Redesign of bootstrap.sh script
    - load configuration file
      (specified as $1 or ./bootstrap_setup or ~/bootstrap_setup)
    - allow proper replacement of submodules with direct clone
    - configurable COLLECTION and SLUGFILE names, COLLECTION_REPO

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,45 +1,73 @@
-#!/bin/sh
+#!/bin/bash
 # bootstrap.sh
 
-# Simple bootstrap script for RedHat family systems (developed on Rocky 9)
+# Bootstrap script for training-vm based systems
+# - reads local settings
 # - installs prerequisites: git, ansible
-# - clones training-collection repo into ./training
-# - sets soft link bootstrap -> training/vm-setup
+# - clones collection repo into collection subdir (with submodules)
+# - creates/links Ansible configuration local.yml
 
-# $1 = repo (default: GitHub)
-
-BOOTSTRAP_DIR="bootstrap"
-COLLECTION_DIR="training"
-COLLECTION_REPO=${1:-"https://github.com/epics-training/training-collection"}
-# supply this environment variable to use a different repository for training-vm
-VM_REPO=${VM_REPO:-none}
+# $1 = settings file (default: ./bootstrap_setup or ~/bootstrap_setup)
 
 if [ "$(whoami)" == "root" ]; then
     echo "This script must be run by a regular user (with sudo privileges)."
     exit 1
 fi
 
-if [ ! -e "/etc/epics-training" ]; then
-    echo "Please specify the slug (short name) of the training course that you"
-    echo "want to configure this machine for."
-    echo "See https://github.com/epics-training/training-collection for valid strings."
-    echo "Leaving it empty will use the latest versions of all available training modules."
-    echo "(This is recommended for development of the training environment.)"
-    read -p "Training course name []: " slug
-    TMP_FILE=$(mktemp -q /tmp/epics.XXXXXX)
-    echo "$slug" > $TMP_FILE
-    sudo cp $TMP_FILE /etc/epics-training
-    sudo chmod 644 /etc/epics-training
+# Associative arrays to configure developer mode
+declare -A REPO BRANCH
+
+# Read configuration file "$1", ./bootstrap_setup or ~/bootstrap_setup
+echo -n "Reading configuration"
+if [ "$1" ]; then
+    if [ -e "$1" ]; then 
+        source "$1"
+        echo " from $1"
+    else
+        echo " [ERROR: file $1 not found]"
+        exit 1
+    fi    
+elif [ -e ./bootstrap_setup ]; then
+    source ./bootstrap_setup
+    echo " from ./bootstrap_setup"
+elif [ -e ~/bootstrap_setup ]; then
+    source ~/bootstrap_setup
+    echo " from ~/bootstrap_setup"
 fi
 
-slug=$(</etc/epics-training)
+# Override these defaults in your 'bootstrap_setup' file
+# Switch submodule $COLLECTION/xyz to direct checkout by setting
+# REPO[xyz] (and BRANCH[xyz]
+COLLECTION=${COLLECTION:-"training"}
+SLUGFILE=${SLUGFILE:-"/etc/epics-training"}
+COLLECTION_REPO=${COLLECTION_REPO:-"https://github.com/epics-training/training-collection"}
+
+# if SLUG is not set, read it from SLUGFILE or ask for it
+if [ ! "${SLUG}" ]; then
+    if [ ! -e "${SLUGFILE}" ]; then
+        echo "Please specify the slug (short name) of the event that you"
+        echo "want to configure this machine for."
+        echo "See ${COLLECTION_REPO} branches for valid strings."
+        echo "Leaving it empty will use the default branch with all available submodules."
+        echo "For development, you can switch submodules to use direct checkouts"
+        echo "through settings in the 'bootstrap_setup' file."
+        echo "The slug will be written to ${SLUGFILE}."
+        read -p "Event name []: " SLUG
+        TMP_FILE=$(mktemp -q /tmp/epics.XXXXXX)
+        echo "$SLUG" > $TMP_FILE
+        sudo cp $TMP_FILE ${SLUGFILE}
+        sudo chmod 644 ${SLUGFILE}
+    else
+        SLUG=$(<${SLUGFILE})
+    fi
+fi
 
 # Install prerequisites: Git, Ansible
 if ! command -v git >/dev/null; then
-    packages="git "
+    packages="git"
 fi
 if ! command -v ansible >/dev/null; then
-    packages="${packages}ansible"
+    packages="${packages} ansible"
 fi
 if [ "${packages}" ]; then
     echo "Installing prerequisites: ${packages}..."
@@ -49,36 +77,56 @@ else
     echo "All prerequisites are installed."
 fi
 
-# Clone the training-collection
-if [ ! -e "${COLLECTION_DIR}" ]; then
+# Clone the collection
+if [ ! -e "${COLLECTION}" ]; then
     branch=""
-    if [ "$slug" ]; then
-        branch="-b $slug"
+    if [ "$SLUG" ]; then
+        branch="-b $SLUG"
     fi
-    echo "Cloning training-collection ($slug) into ./${COLLECTION_DIR}..."
-    git clone --recurse-submodules $branch ${COLLECTION_REPO} ${COLLECTION_DIR}
+    echo "Cloning collection (for $SLUG) into ./${COLLECTION}..."
+    git clone $branch ${COLLECTION_REPO} ${COLLECTION}
 else
-    echo "Update the existing training-vm configuration by running 'git pull' in ./${COLLECTION_DIR}."
-    echo "Switch the training setup with 'git checkout \$(</etc/epics-training)' in ./${COLLECTION_DIR}."
+    echo "Update the existing VM configuration by running 'git pull' in ./${COLLECTION_DIR}."
+    echo "Switch the setup with 'git checkout \$(<${SLUGFILE})' in ./${COLLECTION_DIR}."
 fi
 
-# Set bootstrap soft link
-if [ ! -e "${BOOTSTRAP_DIR}" ]; then
-    ln -s "${COLLECTION_DIR}/vm-setup" "${BOOTSTRAP_DIR}"
-fi
+cd ${COLLECTION}
+modules=$(git submodule | awk '{ print $2 }')
 
-if [ "${VM_REPO}" != "none" ]; then
-    echo "Switching training-vm repository to ${VM_REPO}..."
-    rm -fr ${COLLECTION_DIR}/vm-setup
-    git clone ${VM_REPO} ${COLLECTION_DIR}/vm-setup
-    sed -i ${COLLECTION_DIR}/vm-setup/update.sh -e "/recurse-submodules/d"
-fi
+echo "Adding submodules into ${COLLECTION} ..."
+
+for mod in ${modules}; do
+    repo="${REPO[${mod}]}"
+    branch="${BRANCH[${mod}]}"
+    if [ "$repo" ]; then
+        if [ "$branch" ]; then
+            extra="-b $branch"
+        fi
+        echo "Removing submodule $mod ..."
+        git submodule deinit -f $mod
+        rm -rf .git/modules/$mod $mod
+        git rm -f $mod
+        echo "Cloning $mod from $repo ..."
+        git clone $extra $repo $mod
+    else
+        git submodule update --init $mod
+    fi
+done
 
 # Point out missing local.yml configuration
-if [ ! -e "${COLLECTION_DIR}/vm-setup/ansible/group_vars/local.yml" ]; then
-    ln -s "../../../local.yml" "${COLLECTION_DIR}/vm-setup/ansible/group_vars/local.yml"
+if [ ! -e "vm-setup/ansible/group_vars/local.yml" ]; then
+    if [ ! -e "local.yml" ]; then
+        echo "No local configuration found. Creating one from local.yml.sample"
+        cp "vm-setup/ansible/group_vars/local.yml.sample" "local.yml"
+    fi
+    ln -s "../../../local.yml" "vm-setup/ansible/group_vars/local.yml"
 else
-    echo "Verify your existing local configuration in ${BOOTSTRAP_DIR}/ansible/group_vars/local.yml"
+    echo -n "Verify your existing local configuration"
+    if [ -h "vm-setup/ansible/group_vars/local.yml" ]; then
+        echo " in ${COLLECTION}/local.yml"
+    else
+        echo " in ${COLLECTION}/vm-setup/ansible/group_vars/local.yml"
+    fi
 fi
 
-echo "Run ${BOOTSTRAP_DIR}/update.sh at any time to update your installation."
+echo "Run ${COLLECTION}/vm-setup/update.sh at any time to finish or update your installation."
